4f5743aa261e14cb114148f42f4be9fc6bf589c8
Michael Tews <michael@tews.dev>
2024-11-21 08:34:01 +0100

    feat: added gomod2nix
    Signed-off-by: Michael Tews <michael@tews.dev>

--- flake.nix
+++ flake.nix
@@ -1,29 +1,65 @@
 {
-  description = "Go development environment";
+  description = "nixdevsh";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.0.tar.gz";
+    flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }:
-    let
-      GoVersion = 22;
-
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems (system:
-          f {
-            pkgs = import nixpkgs {
-              inherit system;
-              overlays = [ self.overlays.default ];
-            };
-          });
-    in {
-      overlays.default = final: prev: {
-        go = final."go_1_${toString GoVersion}";
-      };
-
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell { packages = with pkgs; [ go gotools gopls ]; };
-      });
+    gomod2nix = {
+      url = "github:tweag/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, gomod2nix }:
+    flake-utils.lib.eachSystem [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              go = prev.go_1_23;
+              buildGoModule = prev.buildGo123Module;
+            })
+            (self: super: {
+              buildGoApplication = super.callPackage ./builder { };
+            })
+            gomod2nix.overlays.default
+          ];
+        };
+
+        p = pkgs.buildGoApplication {
+          pname = "nixdevsh";
+          version = "0.0.1";
+          src = ./.;
+          pwd = ./.;
+          modules = ./gomod2nix.toml;
+
+          buildInputs = with pkgs; [ go ];
+        };
+
+      in {
+        packages = {
+          default = p;
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src/bin/$pname $out/bin/$path'';
+
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            gopls
+            gotools
+            gomod2nix.packages.${system}.default
+          ];
+        };
+      });
+
 }
+
