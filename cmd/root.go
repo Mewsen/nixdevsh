bf125c5ee05b34c3917fe53d9e12a1892ba8c1cf
Michael Tews <michael@tews.dev>
2024-11-21 12:06:38 +0100

    feat: adds args
    Signed-off-by: Michael Tews <michael@tews.dev>

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"log"
 
+	"github.com/mewsen/nixdevsh/logic"
 	"github.com/mewsen/nixdevsh/ui"
 	"github.com/spf13/cobra"
 )
@@ -13,11 +14,42 @@ var rootCmd = &cobra.Command{
 	Long:  `A fast Nix development shell Generator built with Go.`,
 	Example: `'nixdevsh rust' initiate rust env
 'nixdevsh go --git' initiate go env with git repository`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		err := cobra.OnlyValidArgs(cmd, args)
+		if err != nil {
+			return err
+		}
+
+		err = cobra.RangeArgs(0, 1)(cmd, args)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) < 1 {
+		if len(args) == 0 {
 			ui.Run()
+		} else {
+
+			logic.CreateFlakeFile(args[0])
+			logic.CreateEnvRCInCWD()
+
+			git, err := cmd.PersistentFlags().GetBool("git")
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			if git {
+				logic.InitGitRepository()
+			}
 		}
 	},
+	ValidArgs: logic.DirNamesFromEmbededDir(),
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolP("git", "g", false, "Initialize Git repository")
 }
 
 func Execute() {
