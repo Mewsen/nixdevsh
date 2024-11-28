ebb21f4e74f88605d0846b945f426b966b44be93
Michael Tews <michael@tews.dev>
2024-11-28 14:11:12 +0100

    feat: adds gomod2nix to go flake
    Signed-off-by: Michael Tews <michael@tews.dev>

--- logic/data/go/flake.nix
+++ logic/data/go/flake.nix
@@ -1,43 +1,61 @@
 {
-  description = "Go development shell";
+  description = "Go package";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.0.tar.gz";
+    flake-utils.url = "github:numtide/flake-utils";
+    gomod2nix = {
+      url = "github:nix-community/gomod2nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-
-    flake-utils.lib.eachSystem [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ] (system:
+  outputs = { self, nixpkgs, flake-utils, gomod2nix }:
+    (flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ self.overlays.default ];
-        };
-      in {
-        overlays.default = final: prev: { go = final."go_1_123"; };
-
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            go
-            gotools
-            gopls
-
-            #Nix
-            nixfmt-classic
-            nil
-
-            # For treesitter
-            libgcc
-
-            fd
-            ripgrep
+          overlays = [
+            (final: prev: {
+              go = prev.go_1_23;
+              buildGoModule = prev.buildGo123Module;
+            })
+            (self: super: {
+              buildGoApplication = super.callPackage ./builder { };
+            })
+            gomod2nix.overlays.default
           ];
         };
-      });
+
+        goEnv = pkgs.mkGoEnv { pwd = ./.; };
+
+        p = pkgs.buildGoApplication {
+          pname = "test";
+          version = "0.1";
+          src = ./.;
+          pwd = ./.;
+          modules = ./gomod2nix.toml;
+
+          buildInputs = with pkgs; [ go ];
+        };
+      in {
+
+        packages = {
+          default = p;
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src/bin/$pname $out/bin/$path'';
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            (with pkgs; [ go gopls gotools libgcc fd ripgrep ])
+            goEnv
+            gomod2nix.packages.${system}.default
+          ];
+        };
+      }));
 }
